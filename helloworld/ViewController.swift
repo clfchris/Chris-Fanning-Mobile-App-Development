e84df2243122fce5d1f4ae6f1910dee817beb7da
clfchris <chfa5693@atls221-247-dhcp.colorado.edu>
2014-09-09 10:00:30 -0600

    First Upload
    First Upload of helloWorld as a test of github

---
+++ helloworld/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  helloworld
+//
+//  Created by Christopher Lynn Fanning on 8/28/14.
+//  Copyright (c) 2014 Christopher Lynn Fanning. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    @IBOutlet weak var messageText: UILabel!
+    @IBAction func buttonPressed(sender: UIButton) {
+        messageText.text = "Hello World"
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
