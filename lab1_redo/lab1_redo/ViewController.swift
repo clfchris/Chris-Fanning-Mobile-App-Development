655cbbbe48d4bf6dffb237177a418906eae65ef3
Chris <Chris@rl1-2-208-208-dhcp.int.colorado.edu>
2014-09-24 12:31:43 -0600

    lab 1 review

---
+++ lab1_redo/lab1_redo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lab1_redo
+//
+//  Created by Chris on 9/24/14.
+//  Copyright (c) 2014 Chris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
